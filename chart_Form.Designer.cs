3dd4b3bfb33768a2f4ae5294e916b9a6dfd2486c
나윤성 <rayunseong22@gmail.com>
2024-05-21 15:40:16 +0900

    chart form designer.cs

--- chart_Form.Designer.cs
+++ chart_Form.Designer.cs
@@ -28,36 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rdo_in = new System.Windows.Forms.RadioButton();
-            this.rdo_out = new System.Windows.Forms.RadioButton();
             this.rdo_all = new System.Windows.Forms.RadioButton();
+            this.rdo_out = new System.Windows.Forms.RadioButton();
+            this.rdo_in = new System.Windows.Forms.RadioButton();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.listView1 = new System.Windows.Forms.ListView();
             this.btn_exit = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.btnAddCategory = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(12, 40);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 5);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            this.chart1.Paint += new System.Windows.Forms.PaintEventHandler(this.chart_Form_Paint);
             // 
             // groupBox1
             // 
@@ -71,16 +76,16 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // rdo_in
+            // rdo_all
             // 
-            this.rdo_in.AutoSize = true;
-            this.rdo_in.Location = new System.Drawing.Point(16, 21);
-            this.rdo_in.Name = "rdo_in";
-            this.rdo_in.Size = new System.Drawing.Size(58, 19);
-            this.rdo_in.TabIndex = 0;
-            this.rdo_in.TabStop = true;
-            this.rdo_in.Text = "수입";
-            this.rdo_in.UseVisualStyleBackColor = true;
+            this.rdo_all.AutoSize = true;
+            this.rdo_all.Location = new System.Drawing.Point(144, 24);
+            this.rdo_all.Name = "rdo_all";
+            this.rdo_all.Size = new System.Drawing.Size(58, 19);
+            this.rdo_all.TabIndex = 2;
+            this.rdo_all.TabStop = true;
+            this.rdo_all.Text = "전체";
+            this.rdo_all.UseVisualStyleBackColor = true;
             // 
             // rdo_out
             // 
@@ -93,20 +98,20 @@
             this.rdo_out.Text = "지출";
             this.rdo_out.UseVisualStyleBackColor = true;
             // 
-            // rdo_all
+            // rdo_in
             // 
-            this.rdo_all.AutoSize = true;
-            this.rdo_all.Location = new System.Drawing.Point(144, 24);
-            this.rdo_all.Name = "rdo_all";
-            this.rdo_all.Size = new System.Drawing.Size(58, 19);
-            this.rdo_all.TabIndex = 2;
-            this.rdo_all.TabStop = true;
-            this.rdo_all.Text = "전체";
-            this.rdo_all.UseVisualStyleBackColor = true;
+            this.rdo_in.AutoSize = true;
+            this.rdo_in.Location = new System.Drawing.Point(16, 24);
+            this.rdo_in.Name = "rdo_in";
+            this.rdo_in.Size = new System.Drawing.Size(58, 19);
+            this.rdo_in.TabIndex = 0;
+            this.rdo_in.TabStop = true;
+            this.rdo_in.Text = "수입";
+            this.rdo_in.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 9);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 311);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
             this.dateTimePicker1.TabIndex = 2;
@@ -130,14 +135,41 @@
             this.btn_exit.UseVisualStyleBackColor = true;
             this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
             // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(481, 6);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 25);
+            this.txtValue.TabIndex = 3;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(375, 5);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(100, 25);
+            this.txtCategory.TabIndex = 4;
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.Location = new System.Drawing.Point(587, 8);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(114, 23);
+            this.btnAddCategory.TabIndex = 5;
+            this.btnAddCategory.Text = "카테고리 추가";
+            this.btnAddCategory.UseVisualStyleBackColor = true;
+            this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click);
+            // 
             // chart_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.btnAddCategory);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.chart1);
             this.Name = "chart_Form";
@@ -146,6 +178,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -159,5 +192,8 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Button btnAddCategory;
     }
 }
